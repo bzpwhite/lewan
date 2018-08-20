0d6560f6e1d4890cccea2b4a1ecd1eba56294b61
2515097216 <2515097216@qq.com>
2018-08-20 18:03:16 +0800

    初始化项目

---
+++ html/api/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "sdk",
+  "version": "1.0.0",
+  "description": "Cocos微信小游戏sdk接口文档",
+  "title": "sdk",
+  "url": "",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2018-08-17T10:56:07.755Z",
+    "url": "http://apidocjs.com",
+    "version": "0.17.6"
+  }
+});
